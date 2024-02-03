276d7a677136d14e668f468de9f6278260fbe1c0
Dario Segura <dario.seco@gmail.com>
2024-02-03 17:32:23 -0500

    [TESTER] Initialize app to use develop strapi server

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:pheno_ui/pheno_ui.dart';
 import 'package:pheno_ui_tester/widgets/category_picker.dart';
 
 void main() async {
-  await initializePhenoUi();
+  await initializePhenoUi('https://api.develop.mindora.dev');
   runApp(const AppPhenoUI());
 }
 

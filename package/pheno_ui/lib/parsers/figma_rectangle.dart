242b2248fac932908ba86394cdeae49f3acdd3c8
Dario Segura <dario.seco@gmail.com>
2024-02-07 23:46:47 -0500

    [RECTANGLE] Properly renders rectangles

--- package/pheno_ui/lib/parsers/figma_rectangle.dart
+++ package/pheno_ui/lib/parsers/figma_rectangle.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:mirai/mirai.dart';
+import 'package:pheno_ui/models/figma_simple_style_model.dart';
+import 'package:pheno_ui/parsers/tools/figma_dimensions.dart';
 
-class FigmaRectangleModel {
+import '../widgets/figma_node.dart';
 
-}
-
-class FigmaRectangleParser extends MiraiParser<FigmaRectangleModel> {
+class FigmaRectangleParser extends MiraiParser<FigmaSimpleStyleModel> {
   const FigmaRectangleParser();
 
   @override
-  FigmaRectangleModel getModel(Map<String, dynamic> json) => FigmaRectangleModel();
+  FigmaSimpleStyleModel getModel(Map<String, dynamic> json) => FigmaSimpleStyleModel.fromJson(json);
 
   @override
   String get type => 'figma_rectangle';
 
   @override
-  Widget parse(BuildContext context, FigmaRectangleModel model) {
-    return Container(
-      color: Colors.red,
+  Widget parse(BuildContext context, FigmaSimpleStyleModel model) {
+    Widget widget = Container(
+      decoration: BoxDecoration(
+        color: model.style.color,
+        border: model.style.border,
+        borderRadius: model.style.borderRadius,
+      ),
+      constraints: model.dimensions!.self.sizeConstraints,
     );
+
+    widget = FigmaNode.withContext(context,
+      model: model,
+      child: widget,
+    );
+
+    return dimensionWrapWidget(widget, model.dimensions!, model.parentLayout);
   }
 }

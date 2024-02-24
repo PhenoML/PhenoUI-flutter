ae6a57fc444cf07efb4609f08fde8122db283573
Dario Segura <2651846+darionco@users.noreply.github.com>
2024-02-23 23:05:47 -0500

    [DEMO] Better top bar

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -1,27 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 Widget topBar(BuildContext context, [String? title, void Function()? refresh]) {
   List<Widget> children = [];
 
   if (Navigator.canPop(context)) {
-    children.add(Align(
-      alignment: Alignment.centerLeft,
-      child: IconButton(
-        icon: const Icon(Icons.arrow_back),
-        color: Colors.white,
-        onPressed: () => Navigator.pop(context),
-      ),
+    children.add(IconButton(
+      icon: const Icon(Icons.arrow_back),
+      color: Colors.white,
+      onPressed: () => Navigator.pop(context),
     ));
   }
 
   if (title != null) {
-    children.add(Align(
-      alignment: Alignment.center,
+    children.add(Expanded(
       child: Text(
         title,
+        textAlign: TextAlign.center,
         style: const TextStyle(
           color: Colors.white,
           fontSize: 21,
+          overflow: TextOverflow.ellipsis,
         ),
       ),
     ));
@@ -29,13 +28,10 @@ Widget topBar(BuildContext context, [String? title, void Function()? refresh]) {
 
   if (refresh != null) {
     children.add(
-        Align(
-          alignment: Alignment.centerRight,
-          child: IconButton(
-            icon: const Icon(Icons.refresh),
-            color: Colors.white,
-            onPressed: refresh,
-          ),
+        IconButton(
+          icon: const Icon(Icons.refresh),
+          color: Colors.white,
+          onPressed: refresh,
         )
     );
   }
@@ -44,7 +40,7 @@ Widget topBar(BuildContext context, [String? title, void Function()? refresh]) {
     height: 60,
     color: Colors.blueGrey,
     padding: const EdgeInsets.only(left: 12, right: 52),
-    child: Stack(
+    child: Row(
       children: children,
     ),
   );

bf1e3d7919064f36c9275ee52ece5a00530a3e77
Dario Segura <dario.seco@gmail.com>
2024-02-24 21:08:02 -0500

    [DEMO] Added automatic test sizes to top bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/widgets.dart';
 import 'package:pheno_ui/pheno_ui.dart';
 import 'package:pheno_ui_tester/widgets/category_picker.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
   await initializePhenoUi('https://api.develop.mindora.dev');
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
   runApp(const AppPhenoUI());
 }
 
--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:window_manager/window_manager.dart';
+
+enum ContentSize {
+  iPhone(393, 852),
+  iPhonePlus(430, 932),
+  iPad(834, 1194),
+  iPadPro(1024, 1366),
+  iPadMini(744, 1133),
+  iPadLS(1194, 834),
+  iPadProLS(1366, 1024),
+  iPadMiniLS(1133, 744),
+  ;
+  final double width;
+  final double height;
+  const ContentSize(this.width, this.height);
+}
+
+const double _kTopBarHeight = 60;
 
 Widget topBar(BuildContext context, [String? title, void Function()? refresh, BoxConstraints? constraints]) {
   List<Widget> children = [];
@@ -27,14 +45,48 @@ Widget topBar(BuildContext context, [String? title, void Function()? refresh, Bo
   }
 
   if (constraints != null) {
-    centerChildren.add(Text(
-      '(${constraints!.maxWidth.toInt()}x${constraints!.maxHeight.toInt() - 60})',
-      textAlign: TextAlign.center,
-      style: const TextStyle(
-        color: Colors.white,
-        fontSize: 12,
-        overflow: TextOverflow.ellipsis,
-      ),
+    centerChildren.add(Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        DropdownMenu<ContentSize>(
+          width: 185,
+          inputDecorationTheme: InputDecorationTheme(
+            isDense: true,
+            contentPadding: const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+            constraints: BoxConstraints.tight(const Size.fromHeight(24)),
+          ),
+          textStyle: const TextStyle(
+            color: Colors.white,
+            fontSize: 10,
+          ),
+          trailingIcon: Transform.translate(
+            offset: const Offset(3, -5),
+            child: const Icon(Icons.arrow_drop_down, color: Colors.white),
+          ),
+          onSelected: (ContentSize? value) async {
+            if (value != null) {
+              // get the window diff from the content size
+              var windowSize = await windowManager.getSize();
+              var diff = Size(windowSize.width - constraints.maxWidth, windowSize.height - constraints.maxHeight);
+              windowManager.setSize(
+                  Size(value.width + diff.width, value.height + diff.height + _kTopBarHeight));
+            }
+          },
+          dropdownMenuEntries: ContentSize.values.map((e) => DropdownMenuEntry(
+            value: e,
+            label: '${e.name} (${e.width.toInt()}x${e.height.toInt()})',
+          )).toList(),
+        ),
+        Text(
+          '(${constraints.maxWidth.toInt()}x${constraints.maxHeight.toInt() - _kTopBarHeight.toInt()})',
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ),
+      ]
     ));
   }
 
@@ -56,7 +108,7 @@ Widget topBar(BuildContext context, [String? title, void Function()? refresh, Bo
   }
 
   return Container(
-    height: 60,
+    height: _kTopBarHeight,
     color: Colors.blueGrey,
     padding: const EdgeInsets.only(left: 12, right: 52),
     child: Row(

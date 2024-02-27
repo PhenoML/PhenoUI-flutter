6c06fea063c691bf1356c5726255eed5560ea110
Dario Segura <dario.seco@gmail.com>
2024-02-27 01:02:40 -0500

    [NODE] Figma node model includes user data field

--- package/pheno_ui/lib/models/figma_component_model.dart
+++ package/pheno_ui/lib/models/figma_component_model.dart
@@ -5,11 +5,9 @@ import 'figma_node_model.dart';
 class FigmaComponentModel extends FigmaNodeModel {
   final String widgetType;
   final FigmaLayoutParentValuesModel parentLayout;
-  final Map<String, dynamic> userData;
 
   FigmaComponentModel.fromJson(Map<String, dynamic> json):
     widgetType = json['widgetType'],
     parentLayout = FigmaLayoutParentValuesModel.fromJson(json['parentLayout']),
-    userData = json['__userData'],
     super.fromJson(json);
 }
--- package/pheno_ui/lib/models/figma_node_model.dart
+++ package/pheno_ui/lib/models/figma_node_model.dart
@@ -13,9 +13,12 @@ class FigmaNodeModel {
   final FigmaNodeInfoModel? info;
   final FigmaDimensionsModel? dimensions;
   final Map<String, dynamic>? componentRefs;
+  final Map<String, dynamic> userData;
 
   FigmaNodeModel.fromJson(Map<String, dynamic> json):
     info = json.containsKey('__info') ? FigmaNodeInfoModel.fromJson(json['__info']) : null,
     dimensions = json.containsKey('dimensions') ? FigmaDimensionsModel.fromJson(json['dimensions']) : null,
-    componentRefs = json['componentRefs'];
+    componentRefs = json['componentRefs'],
+    userData = json['__userData'] ?? {}
+  ;
 }

733b86bd4c9133b9e909e8983ee25de330a8fc74
Dario Segura <dario.seco@gmail.com>
2024-02-29 16:53:00 -0500

    [DEBUG] Logger doesn't print time

--- package/pheno_ui/lib/interface/log.dart
+++ package/pheno_ui/lib/interface/log.dart
@@ -3,7 +3,7 @@ Logger get logger => Log.instance;
 
 class Log extends Logger {
   Log._() : super(
-      printer: PrettyPrinter(printTime: true)
+      // printer: PrettyPrinter(printTime: true),
   );
   static final instance = Log._();
 }

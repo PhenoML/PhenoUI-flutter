9dd1e881df59b6caaac3d1baa014a867e96f9f47
Dario Segura <dario.seco@gmail.com>
2024-04-05 22:19:07 -0400

    [ANIMATION] Loads animations, uses animation controller to reuse them

--- package/pheno_ui/lib/parsers/figma_lottie_animation.dart
+++ package/pheno_ui/lib/parsers/figma_lottie_animation.dart
@@ -2,7 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
-import 'package:lottie/src/composition.dart'; // ignore: implementation_imports
+import 'package:pheno_ui/interface/screens.dart';
+import 'package:pheno_ui/models/fimga_image_model.dart';
 import 'package:pheno_ui/parsers/tools/figma_dimensions.dart';
 
 import '../models/figma_lottie_animation_model.dart';
@@ -21,25 +22,7 @@ class FigmaLottieAnimationParser extends MiraiParser<FigmaLottieAnimationModel> 
 
   @override
   Widget parse(BuildContext context, FigmaLottieAnimationModel model) {
-    final bytes = utf8.encode(model.animation);
-
-    Widget widget = Lottie.memory(
-      bytes,
-      fit: BoxFit.cover,
-      animate: model.autoplay,
-      repeat: model.loop,
-      onLoaded: (composition) {
-        var params = CompositionParameters.forComposition(composition);
-        params.startFrame = model.from.toDouble();
-        params.endFrame = model.to.toDouble();
-      },
-    );
-
-    // Widget widget = Lottie.asset(
-    //   'assets/min_pa_anim_sym_unlocked02.json',
-    //   fit: BoxFit.cover,
-    // );
-
+    Widget widget = FigmaLottieAnimation(model: model);
 
     widget = FigmaNode.withContext(context,
       model: model,
@@ -47,5 +30,106 @@ class FigmaLottieAnimationParser extends MiraiParser<FigmaLottieAnimationModel> 
     );
 
     return dimensionWrapWidget(widget, model.dimensions!, model.parentLayout!);
+  }
+}
+
+class FigmaLottieAnimation extends StatefulWidget {
+  final FigmaLottieAnimationModel model;
+
+  const FigmaLottieAnimation({
+    required this.model,
+    super.key,
+  });
+
+  @override
+  State<FigmaLottieAnimation> createState() => _FigmaLottieAnimationState();
+}
+
+class _FigmaLottieAnimationState extends State<FigmaLottieAnimation> with SingleTickerProviderStateMixin {
+  LottieComposition? _composition;
+  late final AnimationController _controller;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+    );
+
+    _initAsync();
+  }
+
+  void _initAsync() async {
+    switch (widget.model.method) {
+      case FigmaImageDataMethod.embed:
+        setState(() {
+          _composition = LottieComposition.parseJsonBytes(utf8.encode(widget.model.animation));
+          _computeControllerSettings();
+        });
+        break;
+
+      default:
+        final composition = await FigmaScreens().provider!.loadAnimation(widget.model.animation);
+        setState(() {
+          _composition = composition;
+          _computeControllerSettings();
+        });
+        break;
+    }
+  }
+
+  void _computeControllerSettings() {
+    if (_composition == null) {
+      return;
+    }
+
+    _controller.duration = _composition!.duration;
+
+    final start = widget.model.from.toDouble() / _composition!.durationFrames;
+    final end = widget.model.to.toDouble() / _composition!.durationFrames;
+
+    if (widget.model.autoplay) {
+      if (widget.model.loop) {
+        _controller.repeat(
+          min: start,
+          max: end,
+          period: _composition!.duration * (end - start),
+        );
+      } else {
+        _controller.value = start;
+        _controller.animateTo(
+          end,
+          duration: _composition!.duration * (end - start)
+        );
+      }
+    } else {
+      _controller.value = start;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget widget;
+    if (_composition != null) {
+      widget = Lottie(
+        composition: _composition!,
+        controller: _controller,
+      );
+    } else {
+      widget = Container();
+    }
+
+    widget = FigmaNode.withContext(context,
+      model: this.widget.model,
+      child: widget,
+    );
+
+    return dimensionWrapWidget(widget, this.widget.model.dimensions!, this.widget.model.parentLayout!);
   }
 }

0936c1fb84d2ad700fc2a2dc2411ab29578b6e85
Dario Segura <dario.seco@gmail.com>
2024-04-17 20:14:41 -0400

    [COMPONENT] Variant model creates empty map on null variantProperties

--- package/pheno_ui/lib/models/figma_component_variant_model.dart
+++ package/pheno_ui/lib/models/figma_component_variant_model.dart
@@ -3,6 +3,6 @@ import 'figma_frame_model.dart';
 class FigmaComponentVariantModel extends FigmaFrameModel {
   final Map<String, dynamic> variantProperties;
   FigmaComponentVariantModel.fromJson(Map<String, dynamic> json):
-        variantProperties = json['variantProperties'],
+        variantProperties = json['variantProperties'] ?? {},
         super.fromJson(json);
 }

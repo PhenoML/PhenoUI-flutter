d9553c267004a4b996d34bf8c074fb04756ba681
Dario Segura <dario.seco@gmail.com>
2024-04-18 12:11:55 -0400

    [NODE] Simple child model specifically returns a FigmaNode

--- package/pheno_ui/lib/models/figma_simple_child_model.dart
+++ package/pheno_ui/lib/models/figma_simple_child_model.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/widgets.dart';
 import 'package:pheno_ui/pheno_ui.dart';
 import '../models/figma_node_model.dart';
+import '../widgets/figma_node.dart';
 
 class FigmaSimpleChildModel extends FigmaNodeModel {
-  final Widget child;
+  final FigmaNode child;
 
   FigmaSimpleChildModel.fromJson(Map<String, dynamic> json):
       child = PhenoUi().fromJson(json['child']),

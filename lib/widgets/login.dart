2132084c22d776d3212f0cd34bdb3ff8ba592ac8
Dario Segura <dario.seco@gmail.com>
2024-04-25 13:37:56 -0400

    [DEMO] Removed unused import

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pheno_ui/interface/strapi.dart';
 import 'package:pheno_ui_tester/widgets/loading_screen.dart';

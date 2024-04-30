671e28df7e494279f0b20d2b41eda907106b9c9a
Dario Segura <2651846+darionco@users.noreply.github.com>
2024-04-30 15:29:18 -0400

    [DEMO] Cleaned up unused imports

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:window_manager/window_manager.dart';
 
 enum ContentSize {

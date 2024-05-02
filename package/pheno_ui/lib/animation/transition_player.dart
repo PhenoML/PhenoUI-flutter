2f8b0c28381569c90e332a2eb467556a856afcbe
Dario Segura <2651846+darionco@users.noreply.github.com>
2024-05-01 21:55:38 -0400

    [ANIMATION] Added fade transition animation (#7)

--- package/pheno_ui/lib/animation/transition_player.dart
+++ package/pheno_ui/lib/animation/transition_player.dart
@@ -13,39 +13,66 @@ class TransitionPlayer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Map<String, dynamic> value = animation.value;
-    Size screenSize = MediaQuery.of(context).size;
-    Offset anchor = value['anchor'] ?? const Offset(0.5, 0.5);
 
-    final Matrix4 translationM4 = Matrix4.identity();
-    final Offset? offset = value['offset'];
-    if (offset != null) {
-      translationM4.translate(offset.dx * screenSize.width, offset.dy * screenSize.height);
+    Matrix4? transform;
+    if (value['offset'] != null || value['rotation'] != null || value['scale'] != null) {
+      Size screenSize = MediaQuery.of(context).size;
+      Offset anchor = value['anchor'] ?? const Offset(0.5, 0.5);
+
+      final Matrix4 translationM4 = Matrix4.identity();
+      final Offset? offset = value['offset'];
+      if (offset != null) {
+        translationM4.translate(
+            offset.dx * screenSize.width, offset.dy * screenSize.height);
+      }
+
+      final Matrix4 rotationM4 = Matrix4.identity();
+      final double? rotation = value['rotation'];
+      if (rotation != null) {
+        final Offset rotationAnchor = value['rotationAnchor'] ?? anchor;
+        rotationM4.translate(screenSize.width * rotationAnchor.dx,
+            screenSize.height * rotationAnchor.dy);
+        rotationM4.rotateZ(rotation);
+        rotationM4.translate(-screenSize.width * rotationAnchor.dx,
+            -screenSize.height * rotationAnchor.dy);
+      }
+
+      final Matrix4 scaleM4 = Matrix4.identity();
+      final double? scale = value['scale'];
+      if (scale != null) {
+        final Offset scaleAnchor = value['scaleAnchor'] ?? anchor;
+        scaleM4.translate(screenSize.width * scaleAnchor.dx,
+            screenSize.height * scaleAnchor.dy);
+        scaleM4.scale(scale);
+        scaleM4.translate(-screenSize.width * scaleAnchor.dx,
+            -screenSize.height * scaleAnchor.dy);
+      }
+
+      transform = translationM4 * rotationM4 * scaleM4;
     }
 
-    final Matrix4 rotationM4 = Matrix4.identity();
-    final double? rotation = value['rotation'];
-    if (rotation != null) {
-      final Offset rotationAnchor = value['rotationAnchor'] ?? anchor;
-      rotationM4.translate(screenSize.width * rotationAnchor.dx, screenSize.height * rotationAnchor.dy);
-      rotationM4.rotateZ(rotation);
-      rotationM4.translate(-screenSize.width * rotationAnchor.dx, -screenSize.height * rotationAnchor.dy);
+    double? opacity;
+    if (value['opacity'] != null) {
+      opacity = value['opacity'];
     }
 
-    final Matrix4 scaleM4 = Matrix4.identity();
-    final double? scale = value['scale'];
-    if (scale != null) {
-      final Offset scaleAnchor = value['scaleAnchor'] ?? anchor;
-      scaleM4.translate(screenSize.width * scaleAnchor.dx, screenSize.height * scaleAnchor.dy);
-      scaleM4.scale(scale);
-      scaleM4.translate(-screenSize.width * scaleAnchor.dx, -screenSize.height * scaleAnchor.dy);
+    Widget widget = child;
+
+    if (transform != null) {
+      widget = Transform(
+        transform: transform,
+        child: widget,
+      );
     }
 
-    final Matrix4 transform = translationM4 * rotationM4 * scaleM4;
+    if (opacity != null) {
+      widget = Opacity(
+        opacity: opacity,
+        child: widget,
+      );
+    }
 
-    return Transform(
-      transform: transform,
-      child: child,
-    );
+    return widget;
   }
 
 }

e2c5e482c0bec37784632e9773148e7af47170c3
Dario Segura <2651846+darionco@users.noreply.github.com>
2024-05-08 15:19:22 -0400

    [FORM] Default button navigation allows for a custom route handler

--- package/pheno_ui/lib/tools/figma_form_handler.dart
+++ package/pheno_ui/lib/tools/figma_form_handler.dart
@@ -7,18 +7,59 @@ import '../interface/route_arguments.dart';
 import 'figma_form_types.dart';
 import 'figma_user_data.dart';
 
+typedef FigmaFormRouteHandler = void Function(String route, RouteArguments arguments);
+
 abstract class FigmaFormHandler {
   const FigmaFormHandler();
-  bool shouldDisplayInput(String id) => true;
-  FutureOr<T?> initialValueForInputID<T>(BuildContext context, String id) => null;
-  void onInputRegistered<T>(BuildContext context, Map<String, FigmaFormInput> inputs, FigmaFormInput<T> input) { /* nothing */ }
-  void onInputValueChanged<T>(BuildContext context, Map<String, FigmaFormInput> inputs, FigmaFormInput<T> input) { /* nothing */ }
-  void onInputEditingComplete<T>(BuildContext context, Map<String, FigmaFormInput> inputs, FigmaFormInput<T> input) { /* nothing */ }
-  void onInputSubmitted<T>(BuildContext context, Map<String, FigmaFormInput> inputs, FigmaFormInput<T> input) { /* nothing */ }
-  void onSubmit(BuildContext context, Map<String, FigmaFormInput> inputs, FigmaUserData userData, String buttonId, Map<String, dynamic>? buttonData);
 
-  static bool handleDefaultButtonNavigation(NavigatorState navigator, FigmaUserData userData, Map<String, dynamic>? buttonData) {
-    if (buttonData != null && buttonData.containsKey('route')){
+  bool shouldDisplayInput(String id) => true;
+
+  FutureOr<T?> initialValueForInputID<T>(
+    BuildContext context,
+    String id
+  ) => null;
+
+  void onInputRegistered<T>(
+    BuildContext context,
+    Map<String, FigmaFormInput> inputs,
+    FigmaFormInput<T> input
+  ) { /* nothing */ }
+
+  void onInputValueChanged<T>(
+    BuildContext context,
+    Map<String, FigmaFormInput> inputs,
+    FigmaFormInput<T> input
+  ) { /* nothing */ }
+
+  void onInputEditingComplete<T>(
+    BuildContext context,
+    Map<String, FigmaFormInput> inputs,
+    FigmaFormInput<T> input
+  ) { /* nothing */ }
+
+  void onInputSubmitted<T>(
+    BuildContext context,
+    Map<String, FigmaFormInput> inputs,
+    FigmaFormInput<T> input
+  ) { /* nothing */ }
+
+  void onSubmit(
+    BuildContext context,
+    Map<String, FigmaFormInput> inputs,
+    FigmaUserData userData,
+    String buttonId,
+    Map<String, dynamic>? buttonData
+  );
+
+  static bool handleDefaultButtonNavigation(
+    NavigatorState navigator,
+    FigmaUserData userData,
+    Map<String, dynamic>? buttonData,
+    {
+      FigmaFormRouteHandler? routeHandler,
+    }
+  ) {
+    if (buttonData != null && buttonData.containsKey('route')) {
       RouteType type = RouteType.values.byNameDefault(buttonData['type'], RouteType.screen);
       String? transitionName = buttonData['transition'];
 
@@ -28,7 +69,9 @@ abstract class FigmaFormHandler {
         data: userData.maybeGet('data'),
       );
 
-      if (arguments.type == RouteType.popup) {
+      if (routeHandler != null) {
+        routeHandler(buttonData['route']!, arguments);
+      } else if (arguments.type == RouteType.popup) {
         navigator.pushNamed(
             buttonData['route']!,
             arguments: arguments

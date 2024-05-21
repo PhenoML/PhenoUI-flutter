d4b2a1783a80e3feab30638489663431e945ed67
Dario Segura <2651846+darionco@users.noreply.github.com>
2024-05-21 12:27:11 -0600

    [WEBVIEW] Is now a stateful widget and supports web

--- package/pheno_ui/lib/widgets/figma_web_view.dart
+++ package/pheno_ui/lib/widgets/figma_web_view.dart
@@ -4,10 +4,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import '../models/figma_node_model.dart';
-import 'stateless_figma_node.dart';
 
-class FigmaWebView extends StatelessFigmaNode<FigmaNodeModel> {
+import '../models/figma_node_model.dart';
+import 'stateful_figma_node.dart';
+
+class FigmaWebView extends StatefulFigmaNode<FigmaNodeModel> {
   const FigmaWebView({required super.model, super.key});
 
   static FigmaWebView fromJson(Map<String, dynamic> json) {
@@ -16,15 +17,31 @@ class FigmaWebView extends StatelessFigmaNode<FigmaNodeModel> {
   }
 
   @override
-  Widget buildFigmaNode(BuildContext context) {
-    Widget widget;
+  StatefulFigmaNodeState createState() => FigmaWebViewState();
+}
 
-    if (!kIsWeb && (Platform.isAndroid || Platform.isIOS)) {
-      var controller = WebViewController()
-        ..setJavaScriptMode(JavaScriptMode.unrestricted)
-        ..setBackgroundColor(const Color(0x00000000))
-        ..loadRequest(Uri.parse(model.userData.get('URL')));
-      return WebViewWidget(controller: controller);
+class FigmaWebViewState extends StatefulFigmaNodeState<FigmaWebView> {
+  WebViewController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (kIsWeb || Platform.isAndroid || Platform.isIOS) {
+      _controller = WebViewController();
+      if (!kIsWeb) {
+        _controller!
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..setBackgroundColor(const Color(0x00000000));
+      }
+      _controller!.loadRequest(Uri.parse(widget.model.userData.get('URL')));
+    }
+  }
+
+  @override
+  Widget buildFigmaNode(BuildContext context) {
+    if (_controller != null) {
+      return WebViewWidget(controller: _controller!);
     }
 
     return Container(

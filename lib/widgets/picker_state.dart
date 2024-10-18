944d27074707e720623553302706df57a9ec723b
Dario Segura <dario@phenoml.com>
2024-10-18 13:38:34 -0400

    [TESTER] Wraps PhenoDataEntry to expose picker list properties

--- lib/widgets/picker_state.dart
+++ lib/widgets/picker_state.dart
@@ -5,16 +5,33 @@ import 'package:pheno_ui_tester/widgets/top_bar.dart';
 
 import 'loading_screen.dart';
 
+enum PickerEntryType {
+  tag,
+  layout,
+}
+
+class PickerEntry {
+  final PhenoDataEntry pde;
+  final PickerEntryType type;
+
+  const PickerEntry(this.pde, this.type);
+
+  String get id => pde.id;
+  String get uid => pde.uid;
+  String get name => pde.name;
+  String get path => pde.data['path'];
+}
+
 abstract class PickerWidget extends StatefulWidget {
   const PickerWidget({super.key});
-  Future<List<PhenoDataEntry>> Function() get getList;
-  Widget Function(PhenoDataEntry, BuildContext, List<PhenoDataEntry>) get builder;
-  Future<void> Function(PhenoDataEntry)? get delete => null;
+  Future<List<PickerEntry>> Function() get getList;
+  Widget Function(PickerEntry, BuildContext, List<PickerEntry>) get builder;
+  Future<void> Function(PickerEntry)? get delete => null;
   String get title;
 }
 
 class PickerState<T extends PickerWidget> extends State<T> {
-  List<PhenoDataEntry>? entries;
+  List<PickerEntry>? entries;
   bool _loading = false;
 
   PickerState();
@@ -39,8 +56,8 @@ class PickerState<T extends PickerWidget> extends State<T> {
     }
 
     List<Widget> children = (entries as List).map((e) => ListTile(
-      leading: const Icon(
-        Icons.screenshot,
+      leading: Icon(
+        e.type == PickerEntryType.tag ? Icons.folder_outlined : Icons.file_open_outlined,
         size: 20,
       ),
       trailing: widget.delete == null? null :IconButton(
